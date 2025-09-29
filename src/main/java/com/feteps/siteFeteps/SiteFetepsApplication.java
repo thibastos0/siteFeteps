cdd4673ca805f8df98eb9205456c1f445999dc7e
thibastos0 <thibastos@live.com>
2025-09-29 09:35:25 -0300

    primeiro git site

---
+++ src/main/java/com/feteps/siteFeteps/SiteFetepsApplication.java
@@ -0,0 +1,13 @@
+package com.feteps.siteFeteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteFetepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteFetepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feteps/siteFeteps/SiteFetepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feteps.siteFeteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteFetepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
